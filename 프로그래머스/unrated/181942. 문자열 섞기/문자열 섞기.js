af2dbacf9be88d05b7db33326397cd2969e010a9
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-08 19:16:54 +0900

    [unrated] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,7 +1,10 @@
 function solution(str1, str2) {
     var answer = '';
+    let first = str1.split('')
+    let second = str2.split('')
+    
     for (i = 0; i < str1.length; i++) {
-        answer += str1[i] + str2[i]
+        answer += first[i] + second[i]
     }
     return answer;
 }
