f9ea2b1387801b8f51618ba1da2128140faf9e4f
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-10 19:06:57 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,13 +1,11 @@
 function solution(n) {
     var answer = 0;
-    let even = []
-    let odd = []
     
-    for (i = 1; i <= parseInt((n+1) / 2); i ++) {
+    for (i = 0; i <= n/2; i++) {
         if (n % 2 === 0) {
             answer += 4 * i * i
         } else {
-            answer += 2 * i - 1
+            answer += 2 * i + 1
         }
     }
     return answer;
