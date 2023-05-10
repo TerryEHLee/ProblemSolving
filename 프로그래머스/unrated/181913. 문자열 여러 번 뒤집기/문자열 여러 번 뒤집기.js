e4029ba322cb968952000007daacad4263da3d17
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-10 19:37:27 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 29.53 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,15 +1,8 @@
 function solution(my_string, queries) {
-    for (var i = 0; i < queries.length; i++) {
-        var start = queries[i][0];
-        var end = queries[i][1];
-        my_string = reverseSubstring(my_string, start, end);
-    }
-    return my_string;
-}
-
-function reverseSubstring(str, start, end) {
-    var left = str.substring(0, start);
-    var middle = str.substring(start, end + 1).split('').reverse().join('');
-    var right = str.substring(end + 1);
-    return left + middle + right;
+    let str = my_string.split('');
+  queries.forEach(([start, end]) => {
+    const changeStr = str.slice(start, end + 1);
+    str.splice(start, changeStr.length, ...changeStr.reverse());
+  });
+  return str.join('');
 }
