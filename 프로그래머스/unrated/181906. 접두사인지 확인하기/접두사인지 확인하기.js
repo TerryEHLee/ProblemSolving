202a5ae05abafeddae0c4e8d79082079380477fd
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-12 10:08:04 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,12 @@
+function solution(my_string, is_prefix) {
+    var answer = 0;
+    let arr = ''
+    
+    for (i = 0; i < my_string.length; i++) {
+        arr += my_string[i]
+        if (arr === is_prefix) {
+            answer = 1
+        }
+    }
+    return answer;
+}
