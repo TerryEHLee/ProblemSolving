608b3b62f0b160956ace1f3f9b26b67c1dbd1e9b
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-12 10:24:12 +0900

    [unrated] Title: 세로 읽기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,10 @@
+function solution(my_string, m, c) {
+    var answer = '';
+    
+    for (i = 0; i < my_string.length; i++) {
+        if (i % m === c-1) {
+            answer += my_string[i]
+        }
+    }
+    return answer;
+}
