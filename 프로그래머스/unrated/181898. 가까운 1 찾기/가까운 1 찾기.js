c1e8beff7ac3d759f9f6fc47077b999d9c575762
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-14 11:32:45 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,11 @@
+function solution(arr, idx) {
+    var answer = -1;
+    
+    for (i = idx; i < arr.length; i++) {
+       if (arr[i] === 1) {
+           answer = i
+           break
+       }
+    }
+    return answer;
+}
