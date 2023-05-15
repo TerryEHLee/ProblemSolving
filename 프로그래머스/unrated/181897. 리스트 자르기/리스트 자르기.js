167636ca5c0126be71fd93b07d8ca513671b4857
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-15 17:25:41 +0900

    [unrated] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,17 @@
+function solution(n, slicer, num_list) {
+  let answer = [];
+
+  if (n === 1) {
+    answer = num_list.slice(0, slicer[1] + 1);
+  } else if (n === 2) {
+    answer = num_list.slice(slicer[0]);
+  } else if (n === 3) {
+    answer = num_list.slice(slicer[0], slicer[1] + 1);
+  } else if (n === 4) {
+    for (let i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+      answer.push(num_list[i]);
+    }
+  }
+
+  return answer;
+}
