514153f4c70e273f5ac446475c9d207593474757
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-22 19:55:26 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 4.82 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,17 +1,21 @@
 function solution(arr, queries) {
-    var answer = [];
+  var answer = [];
 
-    for (var i = 0; i < queries.length; i++) {
-        var array = [];
-        for (var j = queries[i][0]; j <= queries[i][1]; j++) {
-            if (arr[j] > queries[i][2]) {
-                array.push(arr[j]);
-            }
-        }
+  for (i = 0; i < queries.length; i++) {
+    var num = [];
 
-        var minNum = array.length > 0 ? Math.min(...array) : -1;
-        answer.push(minNum);
+    for (j = queries[i][0]; j <= queries[i][1]; j++) {
+      if (arr[j] > queries[i][2]) {
+        num.push(arr[j]);
+      }
     }
 
-    return answer;
+    if (num.length > 0) {
+      answer.push(Math.min(...num));
+    } else {
+      answer.push(-1);
+    }
+  }
+
+  return answer;
 }
