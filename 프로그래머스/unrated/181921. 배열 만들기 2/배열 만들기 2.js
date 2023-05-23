ffb3a61009fdc610e5cb30e3bc616c8373e13082
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-23 19:14:07 +0900

    [unrated] Title: 배열 만들기 2, Time: 20.87 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,21 +1,21 @@
 function solution(l, r) {
     var answer = [];
-    
+
     for (let i = l; i <= r; i++) {
         let str = String(i).split('');
         let hasOnly05 = true;
-        
+
         for (let j = 0; j < str.length; j++) {
             if (str[j] !== '0' && str[j] !== '5') {
                 hasOnly05 = false;
                 break;
             }
         }
-        
+
         if (hasOnly05) {
             answer.push(i);
         }
     }
-    
+
     return answer.length > 0 ? answer : [-1];
 }
