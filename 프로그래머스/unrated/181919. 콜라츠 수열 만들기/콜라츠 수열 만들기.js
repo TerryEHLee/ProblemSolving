de02e91d2fd0ddb861845281bd0e2573bebbe692
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-23 19:22:36 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -3,11 +3,12 @@ function solution(n) {
     
     while (n !== 1) {
         if (n % 2 === 0) {
-            n = n / 2
+            n /= 2
         } else {
-            n = n * 3 + 1
+            n = 3 * n + 1
         }
         answer.push(n)
     }
+    
     return answer;
 }
