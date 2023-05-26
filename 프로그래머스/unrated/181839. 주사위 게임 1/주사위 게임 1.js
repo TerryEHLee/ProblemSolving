133623a9db1371d34a7c9443071736a0277920d5
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-05-26 11:42:54 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    switch (true) {
+        case (a % 2 === 1 && b % 2 === 1):
+            answer = a*a + b*b
+            break
+        case (a % 2 ===0 && b % 2 === 0):
+            answer = Math.abs(a - b)
+            break
+        default:
+            answer = 2 * (a + b)
+    }
+    return answer;
+}
