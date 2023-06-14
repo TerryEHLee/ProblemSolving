5e7de03e8a341d5ac95916ea12317546f36827e5
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-06-14 19:06:24 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,7 +1,5 @@
 function solution(num_list, n) {
     var answer = [];
-    for (i = n - 1; i < num_list.length; i++) {
-        answer.push(num_list[i])
-    }
-    return answer;
+    
+    return answer = num_list.slice(n-1);
 }
