4c352d69643b1c15983df025806f0c47ec8ba181
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-06-28 19:41:51 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,16 +1,15 @@
 function solution(binomial) {
     var answer = 0;
-    var arr = binomial.split(' ')
+    let arr = binomial.split(' ');
     
     switch (arr[1]) {
-        case ('+'): answer = +arr[0] + +arr[2]
-            break
-        case ('-'): answer = arr[0] - arr[2]
-            break
-        case ('*'): answer = arr[0] * arr[2]
-            break
-        case ('/'): answer = arr[0] / arr[2]
-            break
+        case ('+'): answer = +arr[0] + +arr[2]; break;
+        case ('-'): answer = +arr[0] - +arr[2]; break;
+        case ('*'): answer = +arr[0] * +arr[2]; break;
+        case ('/'): answer = +arr[0] / +arr[2]; break;
+        default: answer
     }
+    
+    
     return answer;
 }
