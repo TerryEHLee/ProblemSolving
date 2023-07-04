43bd1c240fe1273d3e9a9533e7116ae3ed16f291
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-07-04 18:59:18 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,12 +1,4 @@
 function solution(num_list) {
     var answer = [];
-    var arr = []
-    
-    arr = num_list.sort((a,b) => a - b)
-    
-    for (i = 0; i < 5; i++) {
-        answer.push(arr[i])
-    }
-    
-    return answer;
+    return num_list.sort((a, b) => a - b).slice(0, 5);
 }
