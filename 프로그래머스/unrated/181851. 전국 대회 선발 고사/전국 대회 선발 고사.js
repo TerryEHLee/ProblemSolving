a91922ecd58a3182f296aa5d1929f0ac8c0c4717
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-07-05 19:24:20 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -2,11 +2,10 @@ function solution(rank, attendance) {
     var answer = 0;
     let arr = [];
     
-    attendance.forEach((bool,i) => {
-        bool && arr.push(rank[i]) 
-    })   
-    arr.sort((a,b) => a - b);
+    attendance.filter((v,i) => v&&arr.push(rank[i]))
+    arr.sort((a,b) => a-b)
     
-    answer = rank.indexOf(arr[0]) * 10000 + rank.indexOf(arr[1]) * 100 + rank.indexOf(arr[2])
+    answer = rank.indexOf(arr[0])*10000 + rank.indexOf(arr[1])*100 + rank.indexOf(arr[2])
+    
     return answer;
 }
