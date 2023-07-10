3fc47b2d2f8593a6fcd116f75e57ac28453da098
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-07-10 19:11:31 +0900

    [unrated] Title: 커피 심부름, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,17 +1,12 @@
 function solution(order) {
     var answer = 0;
     let ame = 0;
-    let latte = 0
+    let latte = 0;
     
     order.forEach(coffee => {
-        if (coffee.includes('americ')) {
-            ame++
-        } else if (coffee.includes('latte')) {
-            latte++            
-        } else {
-            ame++
-        }
+        coffee.includes('americano') ? ame++ :
+        coffee.includes('latte') ? latte++ : ame++
     })
-    answer = ame * 4500 + latte * 5000
+    answer = ame*4500 + latte*5000
     return answer;
 }
