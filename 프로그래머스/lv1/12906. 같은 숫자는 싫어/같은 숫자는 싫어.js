56da51b48d8b0e79b339463363b41fd0207b9a12
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-07-27 11:47:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 22.41 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,24 @@
+// function solution(arr)
+// {
+//     var answer = [arr[0]];
+
+//     for (i = 1; i < arr.length; i++) {
+//         arr[i] !== arr[i-1] ? answer.push(arr[i]) : answer
+//     }
+    
+//     return answer;
+// }
+
+function solution(arr)
+{
+    let beforeNumber
+    const answer = []
+    arr.forEach(num=>{
+        if(beforeNumber !== num){
+            answer.push(num)
+        }
+        beforeNumber = num
+    })
+
+    return answer
+}
