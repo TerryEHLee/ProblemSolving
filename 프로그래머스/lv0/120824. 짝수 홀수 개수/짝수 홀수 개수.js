2bbd49d69eefde3d5ad40ed1c5b0831c07ab1399
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-07-31 09:27:37 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,14 +1,12 @@
 function solution(num_list) {
-    
+    var answer = [];
     let add = 0;
     let odd = 0;
     
-    for (i=0; i < num_list.length; i++) {
-        if (num_list[i] % 2 === 0) {
-            add += 1
-        } else {
-            odd += 1
-        }
-    }
-    return [add, odd];
+    num_list.forEach(num => {
+        num % 2 === 0 ? add++:odd++
+    })
+    answer.push(add)
+    answer.push(odd)
+    return answer;
 }
