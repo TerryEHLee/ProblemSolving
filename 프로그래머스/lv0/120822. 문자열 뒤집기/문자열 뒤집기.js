6bb7d34b8341a93bbe668da794e2a4ad489f88d6
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-07-31 09:13:01 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,8 @@
 function solution(my_string) {
-  var reversed = "";
-  for (var i = my_string.length - 1; i >= 0; i--) {
-    reversed += my_string[i];
-  }
-  return reversed;
+    var answer = '';
+    
+    for (i = my_string.length-1; i >= 0; i--) {
+        answer += my_string[i]
+    }
+    return answer;
 }
