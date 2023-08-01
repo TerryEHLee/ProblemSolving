70299ac5173142c006b08fbc8b4de1b8724afd96
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-01 09:41:05 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,6 +1,6 @@
 function solution(n, k) {
-    let answer = 0;
-    bonus = parseInt(n / 10);
-    answer = 12000 * n + 2000 * (k - bonus);
+    var answer = 0;
+    let service = ~~(n/10)
+    answer = 12000*n + 2000*(k - service)
     return answer;
 }
