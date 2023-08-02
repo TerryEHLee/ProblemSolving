965bab52ff7248a922e996351c2aa10a15db168b
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-02 09:48:51 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.88 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,10 @@
 function solution(n) {
     var answer = 0;
-    for (i = 0; i <= n; i++)  {
-        if (n % i === 0) {
-            answer += 1
-        }
+    
+    for (i = 1; i <= n; i++) {
+       
+            n % i === 0 ? answer++:answer
+       
     }
     return answer;
 }
