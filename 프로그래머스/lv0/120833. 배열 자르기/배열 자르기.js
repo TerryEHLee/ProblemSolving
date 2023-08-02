088db1ace37750f7b4ced518eadf91973ef6bee5
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-02 09:16:04 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,7 +1,6 @@
 function solution(numbers, num1, num2) {
     var answer = [];
-    for (i = num1; i <= num2; i++) {
-        answer.push(numbers[i])
-    }
+    
+    answer = numbers.slice(num1, num2+1)
     return answer;
 }
