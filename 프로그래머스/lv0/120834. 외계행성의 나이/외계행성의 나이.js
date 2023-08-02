e5b0dcb779ccc68b15a6d8bd4ea6d44fb5108751
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-02 09:22:52 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,15 +1,6 @@
 function solution(age) {
-  let answer = '';
-  let str = age.toString()
-    str = str.split('')
-    
-    for (i = 0; i < str.length; i++) {
-        str[i] = parseInt(str[i])
-    }
-    
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    for (num of str) {
-        answer += alphabet[num]
-    }
-  return answer;
+    var answer = '';
+    let alp = 'abcdefghij'
+    age.toString().split('').map(num => answer += alp[num])
+    return answer;
 }
