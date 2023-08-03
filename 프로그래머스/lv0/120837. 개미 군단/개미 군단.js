61e646c1b99cef20c93735b00d69523081733307
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-03 09:15:00 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,14 +1,9 @@
 function solution(hp) {
     var answer = 0;
-    let general = 0
-    let soldier = 0
-    let worker = 0
+    let sol = ~~(hp/5)
+    let bj = ~~((hp-sol*5)/3)
+    let work = hp-(sol*5+bj*3)
     
-    general = parseInt(hp / 5) 
-    soldier = parseInt(((hp - (general * 5))) / 3)
-    worker = hp - general * 5 - soldier * 3
-    
-    answer = general + soldier + worker
-    
+    answer = sol + bj + work
     return answer;
 }
