66665c9b85307475d1cdb511b1252f60b7762f4d
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-04 09:39:03 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,7 @@
 function solution(num_list, n) {
-    var answer = [];
-    for (i = 0; i < num_list.length; i += n) {
-        answer.push(num_list.slice(i, i + n));
+    var answer = [[]];
+    for (i = 0; i < num_list.length; i=i+0) {
+        array.push(num_list.splice(i,n))
     }
     return answer;
 }
