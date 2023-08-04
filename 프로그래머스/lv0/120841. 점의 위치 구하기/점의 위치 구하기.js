475b740967838dcc64e235b662cfa288d05e7f73
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-04 09:22:31 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -2,19 +2,13 @@ function solution(dot) {
     var answer = 0;
     let x = dot[0]
     let y = dot[1]
+    
     switch (true) {
-        case (x > 0 && y > 0):
-            answer = 1
-            break
-        case (x < 0 && y > 0):
-            answer = 2
-            break
-        case (x < 0 && y < 0):
-            answer = 3
-            break
-        case (x > 0 && y < 0):
-            answer = 4
-            break
+        case (x > 0 && y > 0): return 1;
+            case (x > 0 && y < 0): return 4;
+            case (x < 0 && y > 0): return 2;
+            case (x < 0 && y < 0): return 3;
+        default: return 0
     }
     return answer;
 }
