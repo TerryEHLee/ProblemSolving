029e8c5ee70d83868a140bae592262c2f4bd409d
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2023-08-04 10:15:13 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,15 +1,15 @@
 function solution(numbers, direction) {
     var answer = [];
-    if (direction === "right") {
-        answer[0] = numbers[numbers.length - 1];
+    
+    if (direction === 'right') {
+       answer[0] = numbers[numbers.length - 1]
         for (i = 1; i < numbers.length; i++) {
-            answer[i] = numbers[i - 1];
-        }
-    } else if (direction === "left") {
+            answer[i] = numbers[i-1]
+        } }else {
         for (j = 0; j < numbers.length - 1; j++) {
-            answer[j] = numbers[j + 1];
+            answer[j] = numbers[j+1];
+            
         }
-        answer.push(numbers[0]);
-    }
+        answer[numbers.length - 1] = numbers[0]}
     return answer;
 }
