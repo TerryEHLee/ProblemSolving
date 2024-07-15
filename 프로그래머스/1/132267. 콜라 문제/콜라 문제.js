7dc2e48b7ca40359d28154b97461a778e5374e5f
TerryEHLee <130021467+TerryEHLee@users.noreply.github.com>
2024-07-15 11:34:49 +0900

    [level 1] Title: 콜라 문제, Time: 48.97 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,14 @@
+function solution(a, b, n) {
+    var answer = 0;
+    
+    while (n >= a) {
+        answer += Math.floor((n / a)) * b;
+        n = Math.floor((n / a)) * b + n % a
+        
+        console.log('answer==>',answer)
+        console.log('n==>', n)
+    }
+    
+    return answer;
+}
+
